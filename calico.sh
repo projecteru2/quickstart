091d31aa404adbc17322ba8192a6d574e8359bee
CMGS <ilskdw@gmail.com>
2018-06-19 14:06:44 +0800

    use minions

--- calico.sh
+++ calico.sh
@@ -6,20 +6,39 @@ if [[ `whoami` != "root" ]];then
 fi
 
 # calico
-export CALICOCTL_VER=v1.6.1
+export CALICOCTL_VER=v3.1.3
 export NETPOOL=10.213.0.0/16
 export NETNAME="etest"
 curl -L https://github.com/projectcalico/calicoctl/releases/download/${CALICOCTL_VER}/calicoctl -o /usr/bin/calicoctl
 chmod +x /usr/bin/calicoctl
-docker pull calico/node
-ETCD_ENDPOINTS=http://127.0.0.1:2379 calicoctl node run --node-image=calico/node
+
+mkdir -p /etc/calico
+echo "apiVersion: projectcalico.org/v3
+kind: CalicoAPIConfig
+metadata:
+spec:
+  datastoreType: "etcdv3"
+  etcdEndpoints: "http://${ERU_ETCD}"
+" > /etc/calico/calicoctl.cfg
+
+calicoctl node run --node-image=calico/node
 cat << EOF | calicoctl create -f -
-- apiVersion: v1
-  kind: ipPool
+- apiVersion: projectcalico.org/v3
+  kind: IPPool
   metadata:
-    cidr: ${NETPOOL}
+    name: testpool
   spec:
-    nat-outgoing: true
+    natOutgoing: true
+    cidr: ${NETPOOL}
 EOF
+
+# prepare docker plugin
+mkdir -p /etc/eru
+echo "ETCD_ENDPOINTS=http://${ERU_ETCD}" > /etc/eru/minions.conf
+rpm -i https://12-137714834-gh.circle-artifacts.com/0/RPM/eru-minions-0.1-1.el7.x86_64.rpm
+systemctl enable eru-minions
+systemctl start eru-minions
+
 docker network create --driver calico --ipam-driver calico-ipam --subnet ${NETPOOL} ${NETNAME}
 sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.netfilter.nf_conntrack_max=1000000
--- etcd.sh
+++ etcd.sh
@@ -8,11 +8,11 @@ fi
 # etcd
 yum install -y etcd
 # update to 3.2.6+
-export ETCD_VER=v3.2.18
-export DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
+#export ETCD_VER=v3.2.18
+#export DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
 
-curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
-mkdir -p /tmp/etcd
-tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd --strip-components=1
-mv /tmp/etcd/etcd /usr/bin/etcd
-mv /tmp/etcd/etcdctl /usr/bin/etcdctl
+#curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+#mkdir -p /tmp/etcd
+#tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd --strip-components=1
+#mv /tmp/etcd/etcd /usr/bin/etcd
+#mv /tmp/etcd/etcdctl /usr/bin/etcdctl

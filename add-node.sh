9170bd2a74f48b947cc350e906963761c8950fca
zc <zhichuan.liang@shopee.com>
2019-09-13 17:43:52 +0800

    add node with specific node

--- add-node.sh
+++ add-node.sh
@@ -2,13 +2,19 @@ set -u
 : "$ERU_ETCD" "$ERU_CORE" "$NODE_CPU" "$NODE_MEMORY"
 
 . yum.sh
+echo yum ok
 
 . docker.sh
+echo docker ok
 
 . calico.sh
+echo calico ok
 
 . register.sh
+echo register ok
 
 . agent.sh
+echo agent ok
 
 . minions.sh
+echo minion ok

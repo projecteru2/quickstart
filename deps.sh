9f87795e38a9f85f8a78811dcb2e405afdc69cca
CMGS <ilskdw@gmail.com>
2020-05-05 14:23:45 +0800

    detect centos 7 or 8

--- deps.sh
+++ deps.sh
@@ -13,9 +13,11 @@ dist=`./dist.sh`
 if [[ $dist == "centos" ]]; then
   echo "CentOS updating..."
   yum update -y
-  yum install -y epel-release yum-utils device-mapper-persistent-data lvm2 openssl musl-libc-static
+  yum install -y epel-release yum-utils device-mapper-persistent-data lvm2 openssl
   yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/ngompa/musl-libc/repo/epel-7/ngompa-musl-libc-epel-7.repo
+  source /etc/os-release
+  yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/ngompa/musl-libc/repo/epel-$REDHAT_SUPPORT_PRODUCT_VERSION/ngompa-musl-libc-epel-$REDHAT_SUPPORT_PRODUCT_VERSION.repo
+  yum makecache fast
 
 elif [[ $dist == "ubuntu" ]]; then
   echo "Ubuntu updating..."
